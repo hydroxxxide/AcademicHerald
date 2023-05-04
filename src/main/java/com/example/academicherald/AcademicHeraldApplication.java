408ac6ab389d974c3394e972aad5cde25458ee47
Khashem <hashemehraz@gmail.com>
2023-05-05 02:02:13 +0600

    init

---
+++ src/main/java/com/example/academicherald/AcademicHeraldApplication.java
@@ -0,0 +1,13 @@
+package com.example.academicherald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicHeraldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicHeraldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/academicherald/AcademicHeraldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.academicherald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicHeraldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
